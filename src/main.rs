9d61050645db077e3b716252ecc4f4e7c8638a7a
nbari <nbari@tequila.io>
2020-02-06 09:22:43 +0100

    POC

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::{BufRead, BufReader};
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::thread;
+
+fn handle_client(stream: UnixStream) {
+    let stream = BufReader::new(stream);
+    for line in stream.lines() {
+        println!("{}", line.unwrap());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = UnixListener::bind("/tmp/policy-rate-limit.sock").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| handle_client(stream));
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
+    }
 }
