0c7ecbbe3d488cb53fe69efafbb8ea6f7071db9d
nbari <nbari@tequila.io>
2020-02-08 22:54:21 +0100

    mysql, dsn integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::{App, Arg};
+//use dsn;
 use std::error::Error;
 use std::fs::OpenOptions;
 use std::io::Write;
@@ -5,6 +7,73 @@ use std::io::{BufRead, BufReader};
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::process;
 use std::thread;
+
+fn main() {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .arg(
+            Arg::with_name("dsn")
+                .env("DSN")
+                .help("mysql://<username>:<password>@<host>:<port>/<database>")
+                .long("dsn")
+                .short("d")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("socket")
+                .default_value("/tmp/policy-rate-limit.sock")
+                .help("path to Unix domain socket")
+                .long("socket")
+                .short("s"),
+        )
+        .get_matches();
+
+    let socket_path = matches.value_of("socket").unwrap();
+    let dsn = matches.value_of("dsn").unwrap();
+    let dsn = dsn::parse(dsn).unwrap_or_else(|e| {
+        eprintln!("{}", e);
+        process::exit(1);
+    });
+
+    let mut opts = mysql::OptsBuilder::new();
+    opts.user(dsn.username);
+    opts.pass(dsn.password);
+    opts.ip_or_hostname(dsn.host);
+    if let Some(port) = dsn.port {
+        opts.tcp_port(port);
+    }
+    opts.socket(dsn.socket);
+    opts.db_name(dsn.database);
+    //    let opts: mysql::Opts = opts.into();
+    let pool = mysql::Pool::new_manual(1, 3, opts).unwrap_or_else(|e| {
+        eprintln!("Could not connect to MySQL: {}", e);
+        process::exit(1);
+    });
+
+    println!("{:?}", pool);
+    //    println!("{:?}", opts);
+
+    drop(std::fs::remove_file(socket_path));
+    let listener = UnixListener::bind(socket_path).unwrap_or_else(|e| {
+        eprintln!("{}", e);
+        process::exit(1);
+    });
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| match handle_client(stream) {
+                    Err(e) => println!("{}", e),
+                    _ => (),
+                });
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
+    }
+}
 
 fn handle_client(stream: UnixStream) -> Result<(), Box<dyn Error>> {
     let mut reply = stream.try_clone()?;
@@ -23,27 +92,4 @@ fn handle_client(stream: UnixStream) -> Result<(), Box<dyn Error>> {
         file.write_all(format!("{}\n", line).as_bytes())?;
     }
     Ok(())
-}
-
-fn main() {
-    drop(std::fs::remove_file("/tmp/policy-rate-limit.sock"));
-    let listener = UnixListener::bind("/tmp/policy-rate-limit.sock").unwrap_or_else(|e| {
-        eprintln!("{}", e);
-        process::exit(1);
-    });
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(|| match handle_client(stream) {
-                    Ok(_) => println!("dispatched"),
-                    Err(e) => println!("-----> {}", e),
-                });
-            }
-            Err(err) => {
-                println!("Error: {}", err);
-                break;
-            }
-        }
-    }
 }
