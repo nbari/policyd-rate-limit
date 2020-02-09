19c5f0a87668ed91a3103a881204354ef3f051e8
nbari <nbari@tequila.io>
2020-02-09 21:38:13 +0100

    sync Sun Feb  9 21:38:13 CET 2020

---
+++ rate-limit.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS `policyd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE policyd;
+
+CREATE TABLE IF NOT EXISTS `ratelimit` (
+	`email` VARCHAR(128) NOT NULL COMMENT 'sender address (SASL username)',
+	`quota` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'limit',
+	`used` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'current recipient counter',
+	`expiry` INT(10) UNSIGNED DEFAULT '0' COMMENT 'seconds after which the counter gets reset',
+	`rdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'datetime when counter was reset',
+	PRIMARY KEY (`email`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+-- Check if need to reset counter
+-- SELECT IF ((select expiry from ratelimit) > (SELECT TIMESTAMPDIFF(SECOND, t.rdate, NOW()) from ratelimit t), 'ok', 'expired');
+
+-- Check quota
+-- SELECT IF (quota > used, 1, 0) FROM ratelimit WHERE email = "a@foo.com";
